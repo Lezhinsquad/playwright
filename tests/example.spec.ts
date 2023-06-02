bc1bf06dc1dbd71737ecedcf0a95fe22591deb6d
Lezhinsquad <squad@lezhin.com>
2023-06-02 14:56:15 +0900

    test_2

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://q-www.lezhin.com/ko');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/레진코믹스/);
 });
 
 test('get started link', async ({ page }) => {
