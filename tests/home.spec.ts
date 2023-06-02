14c6fe329092c3eada48cb2942c3010bd4f07f41
Lezhinsquad <squad@lezhin.com>
2023-06-02 17:18:38 +0900

    -2test

---
+++ tests/home.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://q-www.lezhin.com/ko'); //레진 KO 홈 접속
+  await page.getByRole('button', { name: '오늘 하루 안보기' }).click(); // 전면배너 오늘 하루 안보기 클릭
+    await page.evaluate(() => { // Top 이동 버튼 노출 되는 위치로 스크롤 내리기
+      window.scrollBy(0, 1000);
+    });
+  await page.getByRole('button', { name: '맨 위로' }).click(); //탑 이동 버튼 클릭
+});
