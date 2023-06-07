aeecdb67bc5925d96dfccbb7d612bc07fb7f7352
Lezhinsquad <squad@lezhin.com>
2023-06-07 17:38:41 +0900

    IF문의 테스트 코드를 변경함

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -7,31 +7,31 @@ test('Top 버튼 이동', async ({ page }) => {
       window.scrollBy(0, 1000);
     });
   
- /* await page.getByRole('button', { name: '맨 위로' }).click(); //탑 이동 버튼 클릭*/
+  await page.waitForTimeout(2000);
+ /*await page.getByRole('button', { name: '맨 위로' }).click(); //탑 이동 버튼 클릭*/
   
   // 최상단으로 이동하여 노출되는 컴포넌트 취향 설정 하기 확인
-  const titleElement = await page.getByRole('button', { name: '취향 설정 하기' }); // 취향설정하기 텍스트를 포함하고있는 p 태그 확인
-  if (titleElement) {
-    const titleText = await titleElement.innerText(); // 
-    if (titleText === '취향 설정 하기') {
-      console.log('취향 설정 하기 텍스트를 찾을 수 없습니다. 스크립트를 확인하세요');
-    } else {
-      console.log('취향 설정 하기 텍스트를 찾아 검증이 완료 되었습니다.');
-    }
-  } else {
-    console.log('취향 설정하기 텍스트 요소가 없습니다.');
-  }
- 
+
   /*
+  const titleElement = await page.getByRole('button', { name: '취향 설정 하기' });
+  const isVisible = await titleElement.isVisible(); 
+  if (isVisible) {
+   console.log('취향 설정 하기 요소를 확인했습니다. 테스트 완료');
+ } else {
+  
+   console.log('취향 설정 하기 테스트 요소가 발견 되지 않습니다. 스크립트를 확인하세요');
+
+ }
+ */
+
   //화면 최상단으로 이동하여 Top 이동 버튼 비노출 검증
   const button = await page.getByRole('button', { name: '맨 위로' });
-  if (button) {
-    button.isVisible;
-    console.log('버튼이 노출 되지 않아 검증이 완료 되었습니다.');
-  } else {
+  const isVisible = await button.isVisible(); 
+  if (isVisible == false) {
     console.log('top 이동 버튼이 노출 됩니다. 스크립트를 확인하세요');
-
+  } else {
+    console.log('버튼이 노출 되지 않아 검증이 완료 되었습니다.');
   }
-*/
+
   
 });
