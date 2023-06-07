32da864a0e0f8c6c9ed12b2276c2936323555a8c
Lezhinsquad <squad@lezhin.com>
2023-06-07 11:52:22 +0900

    test_1

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Top 버튼 이동', async ({ page }) => {
   await page.goto('https://q-www.lezhin.com/ko'); //레진 KO 홈 접속
   await page.getByRole('button', { name: '오늘 하루 안보기' }).click(); // 전면배너 오늘 하루 안보기 클릭
   await page.evaluate(() => { // Top 이동 버튼 노출 되는 위치로 스크롤 내리기
