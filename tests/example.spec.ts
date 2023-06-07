4115d3a6cc41df033fdec821a48a77dc0db3a050
Lezhinsquad <squad@lezhin.com>
2023-06-07 11:52:48 +0900

    test_5

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,31 @@ test('get started link', async ({ page }) => {
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
 });
+
+test('연습하기', async ({ page }) => {
+  
+  await page.goto('https://example.com');
+
+  // 버튼 요소 존재 여부 확인
+  const button = await page.$('#myButton');
+  if (!button) {
+    console.log('Button is not found.');
+  } else {
+    // 버튼 요소의 속성 검증
+    const isHidden = await page.evaluate((btn) => {
+      const styles = getComputedStyle(btn);
+      return styles.display === 'none' || styles.visibility === 'hidden' || styles.opacity === '0';
+    }, button);
+
+    if (isHidden) {
+      console.log('Button is not visible.');
+    } else {
+      console.log('Button is visible.');
+    }
+  }
+
+  
+
+
+
+});
